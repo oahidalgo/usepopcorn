072f5ff1fafe636a89d28345f9d01e73d7c2f6c4
oahidalgo <ohgovilla@gmail.com>
2024-02-06 09:29:20 -0600

    Add - Custom hooks, useRef, useEffect

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 const containerStyle = {
@@ -16,7 +16,7 @@ StarRating.propTypes = {
   defaultRating: PropTypes.number,
   color: PropTypes.string,
   size: PropTypes.number,
-  messages: PropTypes.arrayOf(PropTypes.string),
+  messages: PropTypes.array,
   className: PropTypes.string,
   onSetRating: PropTypes.func,
 };
@@ -39,8 +39,8 @@ export default function StarRating({
   }
 
   const textStyle = {
-    lineHeight: 0,
-    margin: 0,
+    lineHeight: "1",
+    margin: "0",
     color,
     fontSize: `${size / 1.5}px`,
   };
@@ -48,7 +48,7 @@ export default function StarRating({
   return (
     <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
-        {Array.from({ length: maxRating }).map((_, i) => (
+        {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
@@ -112,3 +112,24 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     </span>
   );
 }
+
+/*
+FULL STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#000"
+  stroke="#000"
+>
+  <path
+    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  />
+</svg>
+
+
+EMPTY STAR
+
+
+
+*/
